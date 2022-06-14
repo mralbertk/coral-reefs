afdfe6a9ddfe832440ab5c3878f4c06241e32930
mralberk <mralbertkonrad@gmail.com>
2022-06-14 18:17:13 +0200

    fix(aws): fixed data frame result validation

--- aws/frontend/main.py
+++ aws/frontend/main.py
@@ -310,7 +310,7 @@ if mode == "Export Statistics":
 
     if btn_fetch:
         result_frame = pd.DataFrame(query_result)
-        if result_frame:
+        if not result_frame.empty():
             result_frame.sort_values(['island', 'location', 'year'], inplace=True)
         st.dataframe(result_frame)
         result_csv = result_frame.to_csv().encode('utf-8')
